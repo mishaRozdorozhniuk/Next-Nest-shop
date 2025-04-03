2c6265257ea929a790f03d8a3cb0ce1dd4772dbe
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-04-03 10:07:26 +0200

    added backedn authorization with local and jwt strategies

---
+++ shoppy-backend/src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+import { UsersService } from './users.service';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { TokenPayload } from 'src/auth/token.payload.interface';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  @UseInterceptors(NoFilesInterceptor())
+  createUser(@Body() request: CreateUserRequest) {
+    return this.userService.createUser(request);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getMe(@CurrentUser() user: TokenPayload) {
+    return user;
+  }
+}
